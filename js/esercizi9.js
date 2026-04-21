2a762b14f66ccc2cfddc52031e8daf3a89fff33f
Alessandro <tabakualessandro@gmail.com>
2026-04-21 12:18:14 +0200

    Completato esercizio 1.9: formattazione ricevuta

---
+++ js/esercizi9.js
@@ -0,0 +1,7 @@
+console.log("========================");
+console.log("  RICEVUTA DI PAGAMENTO ");
+console.log("========================");
+console.log("Prodotto: Piatto di Lasagne");
+console.log("Quantità: 2");
+console.log("Prezzo:   18.00 Euro");
+console.log("========================");
