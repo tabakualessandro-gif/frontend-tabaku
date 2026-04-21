8d98cf0fe4ba6107d7bb5397679eaf20e6fff29b
Alessandro <tabakualessandro@gmail.com>
2026-04-21 12:26:21 +0200

    Completato esercizio 1.12: struttura e commenti

---
+++ js/esercizi12.js
@@ -0,0 +1,11 @@
+// SEZIONE 1: Saluti iniziali
+console.log("Benvenuti nel mio programma.");
+console.log("Oggi è una bella giornata.");
+
+// SEZIONE 2: Operazioni matematiche 
+ 
+console.log("L'area di un rettangolo 10x5 è:");
+console.log(10 * 5);
+
+// SEZIONE 3: Messaggio finale 
+console.log("Programma terminato.");
