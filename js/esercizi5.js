b735417b6fe5de825b92716bf64ec03c18861879
Alessandro <tabakualessandro@gmail.com>
2026-04-21 12:11:29 +0200

    Completato esercizio 1.5: commenti nel codice

---
+++ js/esercizi5.js
@@ -0,0 +1,7 @@
+// Questo è un commento su una riga sola (Esercizio 1.5)
+
+/* Questo invece è un commento
+   su più righe, utile per
+   spiegazioni più lunghe.
+*/
+console.log("La differenza tra i commenti è che quelli su più righe possono essere usati per scrivere speigazioni.");
