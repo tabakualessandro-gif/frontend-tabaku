688328aedd1669c1abf27fcc67e5bb4eb80e808c
Alessandro <tabakualessandro@gmail.com>
2026-04-21 12:13:21 +0200

    Completato esercizio su una piccola presentazione

---
+++ js/esercizi6.js
@@ -0,0 +1,4 @@
+console.log("Ciao");
+console.log("Mi chiamo Alessandro Tabaku.");
+console.log("Sto imparando a programmare in JavaScript usando Node.js.");
+console.log("Il mio obiettivo è diventare un professionista del frontend.");
