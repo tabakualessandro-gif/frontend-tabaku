fab54d0bc5864a0fd893891e98496da7b76fdb38
Alessandro <tabakualessandro@gmail.com>
2026-04-21 12:23:29 +0200

    Completato esercizio 1.11:  numeri decimali

---
+++ js/esercizi11.js
@@ -0,0 +1,9 @@
+
+console.log(0.1 + 0.2);
+
+/* OSSERVAZIONE: 
+   Il risultato non è 0.3, ma 0.30.....
+   Questo succede perché i computer memorizzano i numeri in formato binario (0 e 1).
+   Alcuni numeri decimali (come 0.1 e 0.2) diventano frazioni infinite in binario,
+   causando un piccolo errore di arrotondamento.
+*/
