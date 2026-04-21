3300dd4ce40c93a0960be296bed1c1b4947e2806
Alessandro <tabakualessandro@gmail.com>
2026-04-21 12:20:28 +0200

    Completato esercizio 1.10: precedenza degli operatori

---
+++ js/esercizi10.js
@@ -0,0 +1,11 @@
+
+console.log(10 + 5 * 2); 
+
+
+
+console.log((10 + 5) * 2); 
+
+
+/* COMMENTO: I risultati sono diversi perchè senza parentesi, moltiplicazione e divisione vengono eseguite prima di addizione e sottrazione.
+   Le parentesi forzano Node a calcolare prima l'addizione.
+*/
