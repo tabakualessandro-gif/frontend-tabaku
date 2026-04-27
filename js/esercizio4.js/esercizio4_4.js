8d3f8939b2301e0c93459abe31e3f01a9752da7b
Alessandro <tabakualessandro@gmail.com>
2026-04-27 09:50:27 +0200

    Completato esercizio 4.4: calcolo del massimo tra tre numeri

---
+++ js/esercizio4.js/esercizio4_4.js
@@ -0,0 +1,9 @@
+
+const massimo = (a, b, c) => {
+  
+    return Math.max(a, b, c);
+};
+
+
+console.log("Il numero più grande tra 10, 45 e 2 è: " + massimo(10, 45, 2));
+console.log("Il numero più grande tra 100, 50 e 150 è: " + massimo(100, 50, 150));
