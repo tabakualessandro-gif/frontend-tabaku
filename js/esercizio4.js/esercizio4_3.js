aa2fbf37b5d3f6abe90163e8330523dee0dbbade
Alessandro <tabakualessandro@gmail.com>
2026-04-27 09:47:31 +0200

    Completato esercizio 4.3: conversione temperatura

---
+++ js/esercizio4.js/esercizio4_3.js
@@ -0,0 +1,7 @@
+
+const celsiusToFahrenheit = (celsius) => celsius * 9 / 5 + 32;
+
+
+console.log("0°C corrispondono a: " + celsiusToFahrenheit(0) + "°F");   
+console.log("20°C corrispondono a: " + celsiusToFahrenheit(20) + "°F"); 
+console.log("100°C corrispondono a: " + celsiusToFahrenheit(100) + "°F"); 
