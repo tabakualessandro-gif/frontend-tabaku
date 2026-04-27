80459d49779530dfdbc3e61518bd611b09875a60
Alessandro <tabakualessandro@gmail.com>
2026-04-27 10:48:54 +0200

    feat: operatori logici e template literals (Es 2.7-2.8)
    Utilizzo di OR logico e interpolazione di stringhe con backticks.

---
+++ js/esercizio2.js/esercizi2_7.js
@@ -0,0 +1,8 @@
+let eta = 17;
+let haPermesso = true;
+
+
+
+let puoEntrare = (eta >= 18) || haPermesso;
+
+console.log("Può entrare? " + puoEntrare); 
---
+++ js/esercizio2.js/esercizi2_8.js
@@ -0,0 +1,5 @@
+let nome = "Alessandro";
+let cognome = "Tabaku";
+let etaPersona = 20;
+
+console.log(`Ciao, mi chiamo ${nome} ${cognome} e ho ${etaPersona} anni.`);
