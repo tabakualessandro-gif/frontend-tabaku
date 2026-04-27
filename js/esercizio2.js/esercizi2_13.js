6ef52f7aeb37f7266a9c5652b0f95ac4d81fc1e0
Alessandro <tabakualessandro@gmail.com>
2026-04-27 10:51:58 +0200

    feat: calcoli geometrici sul cerchio (Es 2.13)
    Calcolo area e circonferenza utilizzando costanti e Math.PI.

---
+++ js/esercizio2.js/esercizi2_13.js
@@ -0,0 +1,7 @@
+const raggio = 5;
+const PI = Math.PI;
+
+const area = PI * Math.pow(raggio, 2);
+const circonferenza = 2 * PI * raggio;
+
+console.log(`Area: ${area.toFixed(2)}, Circonferenza: ${circonferenza.toFixed(2)}`);
