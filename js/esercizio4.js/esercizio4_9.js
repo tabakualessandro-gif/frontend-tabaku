0002123361122c8c806537cf15aff47c631985ad
Alessandro <tabakualessandro@gmail.com>
2026-04-27 10:03:50 +0200

    Completato esercizio 4.9: potenza con parametri

---
+++ js/esercizio4.js/esercizio4_9.js
@@ -0,0 +1,7 @@
+
+const potenza = (base, esponente = 2) => base ** esponente;
+
+
+console.log("5 elevato a 3 (esponente fornito): " + potenza(5, 3)); 
+console.log("5 elevato a 2 (esponente di default): " + potenza(5)); 
+console.log("10 al quadrato: " + potenza(10));                      
