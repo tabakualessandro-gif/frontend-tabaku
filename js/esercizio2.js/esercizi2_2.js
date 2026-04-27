1141dfa5b33c7fb15772ed52cc39c7de523ec481
Alessandro <tabakualessandro@gmail.com>
2026-04-27 10:42:11 +0200

    feat: operazioni aritmetiche e calcolo sconto (Es 2.2-2.3)
    Pratica con operatori base e calcolo percentuale.

---
+++ js/esercizio2.js/esercizi2_2.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 3;
+
+console.log("Addizione:", a + b);       
+console.log("Sottrazione:", a - b);     
+console.log("Moltiplicazione:", a * b); 
+console.log("Divisione:", a / b);       
+console.log("Modulo:", a % b);          
---
+++ js/esercizio2.js/esercizi2_3.js
@@ -0,0 +1,5 @@
+const prezzo = 49.90;
+const sconto = 20;
+const prezzoScontato = prezzo - (prezzo * sconto / 100);
+
+console.log("Prezzo scontato:", prezzoScontato.toFixed(2)); 
