fe4e31ab3d481e2e68a64b8ee8abf5aca6794aa9
Alessandro <tabakualessandro@gmail.com>
2026-04-27 10:43:35 +0200

    feat: tipi di dati
    Utilizzo di typeof e conversione esplicita da stringa a numero.

---
+++ js/esercizio2.js/esercizi2_4.js
@@ -0,0 +1,11 @@
+let num = 42;
+let str = "Ciao";
+let bool = true;
+let indefinito;
+let nullo = null;
+
+console.log(typeof num);       
+console.log(typeof str);       
+console.log(typeof bool);      
+console.log(typeof indefinito);
+console.log(typeof nullo);     
---
+++ js/esercizio2.js/esercizi2_5.js
@@ -0,0 +1,4 @@
+let stringaNum = "123";
+let numero = Number(stringaNum) + 7;
+
+console.log("Risultato conversione:", numero); 
